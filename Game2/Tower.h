d739eb300e0862be8740245068fd9f8b9aec730b
DireLogomachist <milleram26@gmail.com>
2014-04-05 23:02:47 -0400

    Added Tower class
    Still empty, but more to come

---
+++ Game2/Tower.h
@@ -0,0 +1,29 @@
+#ifndef TOWER_H
+#define TOWER_H
+
+#include "BodyPart.h"
+#include "GameObject.h"
+#include "Box.h"
+
+class Tower {
+
+private:
+
+
+
+
+
+
+public:
+
+
+
+
+
+
+
+
+};
+
+
+#endif
